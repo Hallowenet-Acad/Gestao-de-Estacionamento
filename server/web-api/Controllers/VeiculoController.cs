6bb9558988fc45f2cd6dfa1da419630137e4d4a7
GustavoW <guvieirawalter@gmail.com>
2025-09-16 17:23:23 -0300

    feat: Implementado ViewModels de Veiculo

---
+++ server/web-api/Controllers/VeiculoController.cs
@@ -0,0 +1,31 @@
+﻿using FluentResults;
+using Gestao_de_Estacionamento.Aplicacao.ModuloVeiculo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gestao_de_Estacionamento_web_api.Controllers;
+
+[Route("veiculos")]
+public class VeiculoController : Controller
+{
+    private readonly VeiculoAppService veiculoAppService;
+    
+    public VeiculoController(VeiculoAppService veiculoAppService)
+    {
+        this.veiculoAppService = veiculoAppService;
+    }
+
+    //[HttpGet]
+    //public IActionResult Index()
+    //{
+    //    var resultado = veiculoAppService.SelecionarTodos();
+
+    //    if (resultado.IsFailed)
+    //        return this.RedirecionarParaNotificacaoHome(resultado.ToResult());
+
+    //    var visualizarVM = new VisualizarVeiculosViewModel(resultado.Value);
+
+    //    this.ObterNotificacaoPendente();
+
+    //    return View(visualizarVM);  
+    //}
+}
