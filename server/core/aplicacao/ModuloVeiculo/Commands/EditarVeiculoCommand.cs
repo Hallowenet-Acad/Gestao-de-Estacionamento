6782980d9044036baa238c947c6f392a9a398b1d
GustavoW <guvieirawalter@gmail.com>
2025-09-18 19:32:08 -0300

    feat: Implementado Editar Veiculo Command

---
+++ server/core/aplicacao/ModuloVeiculo/Commands/EditarVeiculoCommand.cs
@@ -0,0 +1,21 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace Gestao_de_Estacionamento.Core.Aplicacao.ModuloVeiculo.Commands;
+
+public record EditarVeiculoCommand (
+    Guid Id,
+    string Placa,
+    string Modelo,
+    string Cor,
+    string CPFHospede
+    ) : IRequest<Result<EditarVeiculoResult>>;
+
+public record EditarVeiculoResult(
+    string Placa,
+    string Modelo,
+    string Cor,
+    string CPFHospede
+    );
+
+
--- server/web-api/Controllers/VeiculoController.cs
+++ server/web-api/Controllers/VeiculoController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using Gestao_de_Estacionamento.Core.Aplicacao.ModuloVeiculo.Commands;
 using Gestao_de_Estacionamento_web_api.Models.ModuloVeiculo;
+using FluentResults;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -15,6 +17,20 @@ public class VeiculoController(IMediator mediator, IMapper mapper) : ControllerB
     [HttpPost]
     public async Task<ActionResult<CadastrarVeiculoResponse>> Cadastrar(CadastrarVeiculoRequest request)
     {
-        var command = mapper.Map(CadastrarVeiculoCommand)
+        var command = mapper.Map<(CadastrarVeiculoCommand)>(request);
+
+        var result = await mediator.Send(command);
+
+        if (result.IsFailed)
+        {
+            if (result.HasError(e => e.HasMetadata("TipoErro", m => m.Equals("RequisicaoInvalida"))))
+            {
+                var errosDeValidacao = result.Errors
+                    .SelectMany(e => e.Reasons.OfType<IError>())
+                    .Select(e => e.Message);
+
+                return BadRequest(errosDeValidacao);
+            }
+        }
     }
 }
