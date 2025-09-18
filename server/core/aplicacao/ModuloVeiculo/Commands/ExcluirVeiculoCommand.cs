fac97855bc48f2befea6fd1c83955c0b878dd206
GustavoW <guvieirawalter@gmail.com>
2025-09-18 19:37:00 -0300

    feat: Implementado Excluir Veiculo Command

---
+++ server/core/aplicacao/ModuloVeiculo/Commands/ExcluirVeiculoCommand.cs
@@ -0,0 +1,20 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace Gestao_de_Estacionamento.Core.Aplicacao.ModuloVeiculo.Commands;
+
+public record ExcluirVeiculoCommand(
+    Guid Id,
+    string Placa, 
+    string Modelo,
+    string Cor,
+    string CPFHospede
+    ) : IRequest<Result<ExcluirVeiculoResult>>;
+
+public record ExcluirVeiculoResult(
+    string Placa,
+    string Modelo,
+    string Cor,
+    string CPFHospede
+    );
+
