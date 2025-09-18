27a6395b00124e8aa25a08ae18e7002037a9577b
GustavoW <guvieirawalter@gmail.com>
2025-09-18 19:47:34 -0300

    feat: Implementado Selecionar Veiculos Query

--- server/core/aplicacao/ModuloVeiculo/Commands/SelecionarVeiculosQuery.cs
+++ server/core/aplicacao/ModuloVeiculo/Commands/SelecionarVeiculosQuery.cs
@@ -1,5 +1,13 @@
-﻿namespace Gestao_de_Estacionamento.Core.Aplicacao.ModuloVeiculo.Commands;
+﻿using FluentResults;
+using MediatR;
+using System.Collections.Immutable;
 
-public record SelecionarVeiculosQuery
-{
-}
+namespace Gestao_de_Estacionamento.Core.Aplicacao.ModuloVeiculo.Commands;
+
+public record SelecionarVeiculosQuery(int? Quantidade) : IRequest<Result<SelecionarVeiculosResult>>;
+
+public record SelecionarVeiculosResult(ImmutableList<SelecionarVeiculosDto> Veiculos);
+
+public record SelecionarVeiculosDto(
+    Guid Id, string Placa, string Modelo, string Cor, string Marca);
+
