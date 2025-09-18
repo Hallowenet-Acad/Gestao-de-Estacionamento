4b95d5e17394ce3cc942f4c124f9f9402c996d43
GustavoW <guvieirawalter@gmail.com>
2025-09-18 18:57:41 -0300

    feat: Implementa Request de Editar Veiculo

---
+++ server/web-api/Models/ModuloVeiculo/EditarVeiculoRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Gestao_de_Estacionamento_web_api.Models.ModuloVeiculo;
+
+public record EditarVeiculoRequest (
+    string Placa,
+    string Modelo,
+    string Cor,
+    string CPFHospede
+    );
+
+public record EditarVeiculoResponse(
+    string Placa,
+    string Modelo,
+    string Cor,
+    string CPFHospede
+    );
