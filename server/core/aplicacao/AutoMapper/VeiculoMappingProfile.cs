6dcf3ab2e978ba927c73ce24555272ace6919340
GustavoW <guvieirawalter@gmail.com>
2025-09-19 01:13:19 -0300

    feat: Implementa Mapping Profile de Veiculo

---
+++ server/core/aplicacao/AutoMapper/VeiculoMappingProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Gestao_de_Estacionamento.Core.Aplicacao.ModuloVeiculo.Commands;
+using Gestao_de_Estacionamento.Core.Dominio.ModuloVeiculo;
+using System.Collections.Immutable;
+
+namespace Gestao_de_Estacionamento.Core.Aplicacao.AutoMapper;
+
+public class VeiculoMappingProfile : Profile
+{
+    public VeiculoMappingProfile()
+    {
+        CreateMap<CadastrarVeiculoCommand, Veiculo>();
+        CreateMap<Veiculo, CadastrarVeiculoResult>();
+
+        CreateMap<EditarVeiculoCommand, Veiculo>();
+        CreateMap<Veiculo, EditarVeiculoResult>();
+
+        CreateMap<Veiculo, SelecionarVeiculoPorIdResult>()
+            .ConvertUsing(src => new SelecionarVeiculoPorIdResult(
+                src.Id,
+                src.Placa,
+                src.Modelo,
+                src.Cor,
+                src.CPFHospede
+                ));
+
+        CreateMap<Veiculo, SelecionarVeiculosDto>();
+
+        CreateMap<IEnumerable<Veiculo>, SelecionarVeiculosResult>()
+            .ConvertUsing((src, dest, ctx) =>
+            new SelecionarVeiculosResult(
+                src?.Select(c => ctx.Mapper.Map<SelecionarVeiculosDto>(c)).ToImmutableList() ?? ImmutableList<SelecionarVeiculosDto>.Empty
+                )
+            );
+    }
+}
