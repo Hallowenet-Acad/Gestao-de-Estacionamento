50a203055d9166b90f4434840b07ea264e72e35c
GustavoW <guvieirawalter@gmail.com>
2025-09-18 23:01:29 -0300

    fix: Corrigido Excluir Veiculo Command

--- server/core/aplicacao/ModuloVeiculo/Commands/ExcluirVeiculoCommand.cs
+++ server/core/aplicacao/ModuloVeiculo/Commands/ExcluirVeiculoCommand.cs
@@ -3,18 +3,7 @@ using MediatR;
 
 namespace Gestao_de_Estacionamento.Core.Aplicacao.ModuloVeiculo.Commands;
 
-public record ExcluirVeiculoCommand(
-    Guid Id,
-    string Placa, 
-    string Modelo,
-    string Cor,
-    string CPFHospede
-    ) : IRequest<Result<ExcluirVeiculoResult>>;
+public record ExcluirVeiculoCommand(Guid Id) : IRequest<Result<ExcluirVeiculoResult>>;
 
-public record ExcluirVeiculoResult(
-    string Placa,
-    string Modelo,
-    string Cor,
-    string CPFHospede
-    );
+public record ExcluirVeiculoResult();
 
---
+++ server/core/aplicacao/ModuloVeiculo/Handlers/ExcluirVeiculoCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using FluentResults;
+using FluentValidation;
+using Gestao_de_Estacionamento.Core.Aplicacao.Compartilhado;
+using Gestao_de_Estacionamento.Core.Aplicacao.ModuloVeiculo.Commands;
+using Gestao_de_Estacionamento.Core.Dominio.Compartilhado;
+using Gestao_de_Estacionamento.Core.Dominio.ModuloAutenticacao;
+using Gestao_de_Estacionamento.Core.Dominio.ModuloVeiculo;
+using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+
+namespace Gestao_de_Estacionamento.Core.Aplicacao.ModuloVeiculo.Handlers;
+
+public class ExcluirVeiculoCommandHandler(
+   IRepositorioVeiculo repositorioVeiculo,
+    ITenantProvider tenantProvider,
+    IUnitOfWork unitOfWork,
+    IDistributedCache cache,
+    ILogger<ExcluirVeiculoCommandHandler> logger
+    ) : IRequestHandler<ExcluirVeiculoCommand, Result<ExcluirVeiculoResult>>
+{
+    public async Task<Result<ExcluirVeiculoResult>> Handle(ExcluirVeiculoCommand command, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var veiculoSelecionado = await repositorioVeiculo.SelecionarRegistroPorIdAsync(command.Id);
+
+            if (veiculoSelecionado is null)
+                return Result.Fail(ResultadosErro.RegistroNaoEncontradoErro(command.Id));
+
+            await repositorioVeiculo.ExcluirRegistroAsync(command.Id);
+
+            await unitOfWork.CommitAsync();
+
+            var cacheKey = $"veiculos:u={tenantProvider.UsuarioId.GetValueOrDefault()}:q=all";
+
+            await cache.RemoveAsync(cacheKey, cancellationToken);
+
+            var result = new ExcluirVeiculoResult();
+
+            return Result.Ok(result);
+        }
+    }
+}
