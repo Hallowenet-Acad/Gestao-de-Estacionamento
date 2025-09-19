3264a2907800caa79919e840b017540ac2596545
GustavoW <guvieirawalter@gmail.com>
2025-09-19 00:39:20 -0300

    feat: Implementado Cadastrar Veiculo Command Validator

---
+++ server/core/aplicacao/FluentValidation/ModuloVeiculo/CadastrarVeiculoCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Gestao_de_Estacionamento.Core.Aplicacao.ModuloVeiculo.Commands;
+
+namespace Gestao_de_Estacionamento.Core.Aplicacao.FluentValidation.ModuloVeiculo;
+
+public class CadastrarVeiculoCommandValidator : AbstractValidator<CadastrarVeiculoCommand>
+{
+    public CadastrarVeiculoCommandValidator()
+    {
+        RuleFor(x => x.Placa)
+            .NotEmpty().WithMessage("A placa do veículo é obrigatória.")
+            .MinimumLength(7).WithMessage("A placa do veículo deve ter no mínimo 7 caracteres.")
+            .MaximumLength(7).WithMessage("A placa do veículo deve ter no máximo 7 caracteres.");
+        RuleFor(x => x.Modelo)
+            .NotEmpty().WithMessage("O modelo do veículo é obrigatório.")
+            .MinimumLength(2).WithMessage("O modelo do veículo deve ter no mínimo 2 caracteres.")
+            .MaximumLength(50).WithMessage("O modelo do veículo deve ter no máximo 50 caracteres.");
+        RuleFor(x => x.Cor)
+            .NotEmpty().WithMessage("A cor do veículo é obrigatória.")
+            .MinimumLength(2).WithMessage("A cor do veículo deve ter no mínimo 2 caracteres.")
+            .MaximumLength(20).WithMessage("A cor do veículo deve ter no máximo 20 caracteres.");
+        RuleFor(x => x.CPFHospede)
+            .NotEmpty().WithMessage("O CPF do hóspede é obrigatório.")
+            .Matches(@"^\d{11}$").WithMessage("O CPF do hóspede deve conter exatamente 11 dígitos numéricos.");
+    }
+}
