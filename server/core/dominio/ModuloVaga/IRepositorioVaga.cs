63a3dfa36b1397667b5371b56b27d241e579e089
GustavoW <guvieirawalter@gmail.com>
2025-09-19 17:21:41 -0300

    feat: Implementa Modulo de vaga na camada de dominio

---
+++ server/core/dominio/ModuloVaga/IRepositorioVaga.cs
@@ -0,0 +1,5 @@
+﻿using Gestao_de_Estacionamento.Core.Dominio.Compartilhado;
+
+namespace Gestao_de_Estacionamento.Core.Dominio.ModuloVaga;
+
+public interface IRepositorioVaga : IRepositorio<Vaga>;
---
+++ server/core/dominio/ModuloVaga/Vaga.cs
@@ -0,0 +1,44 @@
+﻿using Gestao_de_Estacionamento.Core.Dominio.Compartilhado;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Gestao_de_Estacionamento.Core.Dominio.ModuloVaga;
+
+public class Vaga : EntidadeBase<Vaga>
+{
+    public int Numero { get; set; }
+    public char Zona { get; set; }
+    public bool Status { get; set; } // true = ocupada, false = livre
+
+
+    [ExcludeFromCodeCoverage]
+    public Vaga() { }
+
+    public Vaga(int numero) : this()
+    {
+        Id = Guid.NewGuid();
+        Numero = numero;
+    }
+
+    public Vaga(int numero, char zona, bool status) : this(numero)
+    {
+        Numero = numero;
+        Zona = zona;
+        Status = status;
+    }
+
+    public override void AtualizarRegistro(Vaga registroAtualizado)
+    {
+        Numero = registroAtualizado.Numero;
+        Zona = registroAtualizado.Zona;
+        Status = registroAtualizado.Status;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Vaga vaga &&
+               Id == vaga.Id &&
+               Numero == vaga.Numero &&
+               Zona == vaga.Zona &&
+               Status == vaga.Status;
+    }
+}
