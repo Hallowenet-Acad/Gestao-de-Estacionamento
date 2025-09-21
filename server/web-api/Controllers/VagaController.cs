b14d8dc00eb2573850d78af64462f49eb9e1e00e
GustavoW <guvieirawalter@gmail.com>
2025-09-21 02:07:24 -0300

    feat: Implementa Controller de vaga

--- server/web-api/Controllers/VagaController.cs
+++ server/web-api/Controllers/VagaController.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using FluentResults;
+using Gestao_de_Estacionamento.Core.Aplicacao.ModuloVaga.Commands;
+using Gestao_de_Estacionamento_web_api.Models.ModuloVaga;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,5 +14,89 @@ namespace Gestao_de_Estacionamento_web_api.Controllers;
 public class VagaController(IMediator mediator, IMapper mapper) : ControllerBase
 {
     [HttpPost]
-    public async Task<ActionResult<CadastrarVagaRe>>
+    public async Task<ActionResult<CadastrarVagaResponse>> Cadastrar(CadastrarVagaRequest request)
+    {
+        var command = mapper.Map<CadastrarVagaCommand>(request);
+
+        var result = await mediator.Send(command);
+
+        if (result.IsFailed)
+        {
+            if (result.HasError(e => e.HasMetadata("TipoErro", m => m.Equals("RequisicaoInvalida"))))
+            {
+                var errosDeValidacao = result.Errors
+                    .SelectMany(e => e.Reasons.OfType<IError>())
+                    .Select(e => e.Message);
+
+                return BadRequest(errosDeValidacao);
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        var response = mapper.Map<CadastrarVagaResponse>(result.Value);
+
+        return Created(string.Empty, response);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult<EditarVagaResponse>> Editar(Guid id, EditarVagaRequest request)
+    {
+        var command = mapper.Map<(Guid, EditarVagaRequest), EditarVagaCommand>((id, request));
+   
+        var result = await mediator.Send(command);
+
+        if (result.IsFailed)
+            return BadRequest();
+
+        var response = mapper.Map<EditarVagaResponse>(result.Value);
+
+        return Ok(response);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult<ExcluirVagaResponse>> Excluir(Guid id)
+    {
+        var command = mapper.Map<ExcluirVagaCommand>(id);
+
+        var result = await mediator.Send(command);
+
+        if (result.IsFailed)
+            return BadRequest();
+
+        return NoContent();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<SelecionarVagasResponse>> SelecionarRegistros(
+        [FromQuery] SelecionarVagasRequest? request,
+        CancellationToken cancellationToken
+        )
+    {
+        var query = mapper.Map<SelecionarVagasQuery>(request);
+
+        var result = await mediator.Send(query, cancellationToken);
+
+        if (result.IsFailed)
+            return BadRequest();
+
+        var response = mapper.Map<SelecionarVagasResponse>(result.Value);
+
+        return Ok(Response);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<SelecionarVagaPorIdResponse>> SelecionarRegistroPorId(Guid id)
+    {
+        var query = mapper.Map<SelecionarVagaPorIdQuery>(id);
+
+        var result = await mediator.Send(query);
+
+        if (result.IsFailed)
+            return NotFound(id);
+
+        var response = mapper.Map<SelecionarVagaPorIdResponse>(result.Value);
+
+        return Ok(response);
+    }
 }
